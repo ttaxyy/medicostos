370728ddbb1a2b2cc9a9c4d24e11482f28d45941
gpavezb <gonzalo.pavezb@gmail.com>
2024-07-05 21:03:55 -0400

    Arreglo sin solucionar en card Gonzalo Pavez

---
+++ medicostos/src/assets/Lista.js
@@ -0,0 +1,195 @@
+let Lista = [
+  {
+    nombre: "Paracetamol 500Mg X 16 comprimidos (LCh)",
+    complemento: "",
+    precio: 390.0,
+    urlImagen:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2019/07/paracetamol-chile-1.jpg",
+    urlMedicamento:
+      "https://www.ecofarmacias.cl/producto/paracetamol-500mg-x-16-2/",
+    farmaciaId: 4,
+    farmaciaNombre: "Eco Farmacias",
+    farmaciaDireccion: "Eco Farmacias",
+    farmaciaTelefono: "+56 9 74472812",
+    farmaciaUrlImg:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2018/08/logo_eco_new.webp",
+    farmaciaUrlWeb: "https://www.ecofarmacias.cl/",
+  },
+  {
+    nombre: "Paracetamol 500 Mg X 16 comprimidos (Andrómaco)",
+    complemento: "",
+    precio: 390.0,
+    urlImagen:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2019/07/paracetamol-500x16-1-1.jpg",
+    urlMedicamento:
+      "https://www.ecofarmacias.cl/producto/paracetamol-500mg-x-16/",
+    farmaciaId: 4,
+    farmaciaNombre: "Eco Farmacias",
+    farmaciaDireccion: "Eco Farmacias",
+    farmaciaTelefono: "+56 9 74472812",
+    farmaciaUrlImg:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2018/08/logo_eco_new.webp",
+    farmaciaUrlWeb: "https://www.ecofarmacias.cl/",
+  },
+  {
+    nombre: "Tapsin Compuesto Dia 5 g x 1 Sobre Polvo Para Solución Oral",
+    complemento: "Maver S.A.",
+    precio: 745.0,
+    urlImagen:
+      "https://www.farmaciasahumada.cl/dw/image/v2/BJVH_PRD/on/demandware.static/-/Sites-ahumada-master-catalog/default/dw59e93d02/images/products/9235/9235.jpg?sw=247&sh=247&sm=fit",
+    urlMedicamento:
+      "https://www.farmaciasahumada.clhttps://www.farmaciasahumada.cl/tapsin-compuesto-dia-5-g-x-1-sobre-polvo-para-solucion-oral-9235.html",
+    farmaciaId: 1,
+    farmaciaNombre: "Farmacias Ahumada",
+    farmaciaDireccion: "Farmacias Ahumada",
+    farmaciaTelefono: "600 222 4000",
+    farmaciaUrlImg:
+      "https://www.farmaciasahumada.cl/on/demandware.static/Sites-ahumada-cl-Site/-/default/dwc7cc806b/images/logo.svg",
+    farmaciaUrlWeb: "https://www.farmaciasahumada.cl/",
+  },
+  {
+    nombre: "PARACETAMOL 500 MG X 16 COMPRIMIDOS",
+    complemento: "",
+    precio: 790.0,
+    urlImagen:
+      "https://farmaciaschilespa.cl/wp-content/uploads/2022/07/PARACETAMOL-500-16-1024x1024.jpg",
+    urlMedicamento:
+      "https://farmaciaschilespa.cl/index.php/producto/paracetamol-500-mg-x-16-comprimidos/",
+    farmaciaId: 5,
+    farmaciaNombre: "Farmacias Chile",
+    farmaciaDireccion: "Farmacias Chile",
+    farmaciaTelefono: "+56 9 7525 9804",
+    farmaciaUrlImg:
+      "https://farmaciaschilespa.cl/wp-content/uploads/2022/06/LOGO-FC-HORIZONTAL1.png",
+    farmaciaUrlWeb: "https://farmaciaschilespa.cl/",
+  },
+  {
+    nombre: "PARACETAMOL 500mg Comp. x 16",
+    complemento: "",
+    precio: 890.0,
+    urlImagen:
+      "https://cdn.shopify.com/s/files/1/0581/6687/0176/products/7800018163338_PARACETAMOL_500mg_Compx16.jpg?v=1655929186&width=2084",
+    urlMedicamento:
+      "https://farmacialabotika.cl/products/paracetamol-com-500mg-x16andromaco-genericos",
+    farmaciaId: 6,
+    farmaciaNombre: "Farmacia La Botika",
+    farmaciaDireccion: "Farmacia La Botika",
+    farmaciaTelefono: "+569 9132 2673",
+    farmaciaUrlImg:
+      "https://farmacialabotika.cl/cdn/shop/files/LaBotika_Color_648cb663-e8ff-488b-a5d9-4c5ebe3c5045_205x@2x.png?v=1630001196",
+    farmaciaUrlWeb: "https://farmacialabotika.cl/",
+  },
+  {
+    nombre: "PARACETAMOL",
+    complemento: "PARACETAMOL (B) 16 COMPRIMIDOS",
+    precio: 999.0,
+    urlImagen:
+      "https://static.salcobrandonline.cl/spree/products/36317/small/36333.jpg?1641477363",
+    urlMedicamento:
+      "https://salcobrand.cl/products/paracetamol-b-500mg-x16com?default_sku=36333&queryID=23258a533318fc54fcd44386e4a4c9c1",
+    farmaciaId: 2,
+    farmaciaNombre: "Salcobrand",
+    farmaciaDireccion: "Salcobrand",
+    farmaciaTelefono: "600 360 6000",
+    farmaciaUrlImg:
+      "https://static.salcobrandonline.cl/assets/logo-73fe73eb9cf65adf981684077f38a616190d7759b74439763a45b9b985fc36e5.svg",
+    farmaciaUrlWeb: "https://salcobrand.cl/",
+  },
+  {
+    nombre: "Paracetamol 100 mg/ml Gotas 15mL (Valma)",
+    complemento: "",
+    precio: 1190.0,
+    urlImagen:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2019/07/paracetamol-infantil-15-ml.jpg",
+    urlMedicamento: "https://www.ecofarmacias.cl/producto/paracetamol-gotas/",
+    farmaciaId: 4,
+    farmaciaNombre: "Eco Farmacias",
+    farmaciaDireccion: "Eco Farmacias",
+    farmaciaTelefono: "+56 9 74472812",
+    farmaciaUrlImg:
+      "https://www.ecofarmacias.cl/wp-content/uploads/2018/08/logo_eco_new.webp",
+    farmaciaUrlWeb: "https://www.ecofarmacias.cl/",
+  },
+  {
+    nombre: "PARACETAMOL",
+    complemento: "PARACETAMOL (B) 16 COMPRIMIDOS",
+    precio: 1199.0,
+    urlImagen:
+      "https://static.salcobrandonline.cl/spree/products/28695/small/432432.jpg?1641477870",
+    urlMedicamento:
+      "https://salcobrand.cl/products/paracetamol-500mg-de-16-comprimidos-7d64a843-389b-4155-8a75-a284ec7f7b94?default_sku=432432&queryID=0640ea6ac763f6656b218de91059125a",
+    farmaciaId: 2,
+    farmaciaNombre: "Salcobrand",
+    farmaciaDireccion: "Salcobrand",
+    farmaciaTelefono: "600 360 6000",
+    farmaciaUrlImg:
+      "https://static.salcobrandonline.cl/assets/logo-73fe73eb9cf65adf981684077f38a616190d7759b74439763a45b9b985fc36e5.svg",
+    farmaciaUrlWeb: "https://salcobrand.cl/",
+  },
+  {
+    nombre: "PARACETAMOL MINTLAB 80mg Comp. x 20",
+    complemento: "",
+    precio: 1490.0,
+    urlImagen:
+      "https://cdn.shopify.com/s/files/1/0581/6687/0176/products/7800063111001_PARACETAMOL_MINTLAB_80MG_COMP_X20_jpg_Mesadetrabajo1.jpg?v=1652841092&width=2084",
+    urlMedicamento:
+      "https://farmacialabotika.cl/products/paracetamol-mintlab-80mg-comp-x-20",
+    farmaciaId: 6,
+    farmaciaNombre: "Farmacia La Botika",
+    farmaciaDireccion: "Farmacia La Botika",
+    farmaciaTelefono: "+569 9132 2673",
+    farmaciaUrlImg:
+      "https://farmacialabotika.cl/cdn/shop/files/LaBotika_Color_648cb663-e8ff-488b-a5d9-4c5ebe3c5045_205x@2x.png?v=1630001196",
+    farmaciaUrlWeb: "https://farmacialabotika.cl/",
+  },
+  {
+    nombre: "BPH",
+    complemento: "AGUA PARA INYECTABLES 10ML 5 AMPOLLAS",
+    precio: 1499.0,
+    urlImagen:
+      "https://static.salcobrandonline.cl/spree/products/76478/small/577201.jpg?1659111593",
+    urlMedicamento:
+      "https://salcobrand.cl/products/agua-para-inyectables-10ml-5-ampollas?default_sku=577201&queryID=049e49782816a065c09a407c7830fe8e",
+    farmaciaId: 2,
+    farmaciaNombre: "Salcobrand",
+    farmaciaDireccion: "Salcobrand",
+    farmaciaTelefono: "600 360 6000",
+    farmaciaUrlImg:
+      "https://static.salcobrandonline.cl/assets/logo-73fe73eb9cf65adf981684077f38a616190d7759b74439763a45b9b985fc36e5.svg",
+    farmaciaUrlWeb: "https://salcobrand.cl/",
+  },
+  {
+    nombre: "BPH",
+    complemento: "AGUA PARA INYECTABLES 5ML 5 AMPOLLAS",
+    precio: 1499.0,
+    urlImagen:
+      "https://static.salcobrandonline.cl/spree/products/76476/small/577214.jpg?1659111568",
+    urlMedicamento:
+      "https://salcobrand.cl/products/agua-para-inyectables-5ml-5-ampollas?default_sku=577214&queryID=fdceb2217a2b650b37532151f568c2fd",
+    farmaciaId: 2,
+    farmaciaNombre: "Salcobrand",
+    farmaciaDireccion: "Salcobrand",
+    farmaciaTelefono: "600 360 6000",
+    farmaciaUrlImg:
+      "https://static.salcobrandonline.cl/assets/logo-73fe73eb9cf65adf981684077f38a616190d7759b74439763a45b9b985fc36e5.svg",
+    farmaciaUrlWeb: "https://salcobrand.cl/",
+  },
+  {
+    nombre: "PARACETAMOL VALMA 10% Gotas x 15ml",
+    complemento: "",
+    precio: 1590.0,
+    urlImagen:
+      "https://cdn.shopify.com/s/files/1/0581/6687/0176/products/7800044000270_PARACETAMOL_VALMA_10__GOTASX15ML.jpg?v=1653142674&width=2084",
+    urlMedicamento:
+      "https://farmacialabotika.cl/products/paracetamol-valma-10-gotas-x-15ml",
+    farmaciaId: 6,
+    farmaciaNombre: "Farmacia La Botika",
+    farmaciaDireccion: "Farmacia La Botika",
+    farmaciaTelefono: "+569 9132 2673",
+    farmaciaUrlImg:
+      "https://farmacialabotika.cl/cdn/shop/files/LaBotika_Color_648cb663-e8ff-488b-a5d9-4c5ebe3c5045_205x@2x.png?v=1630001196",
+    farmaciaUrlWeb: "https://farmacialabotika.cl/",
+  },
+];
+
+export default Lista;
