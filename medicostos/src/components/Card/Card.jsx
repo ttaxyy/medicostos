aff5187b65b5320b9ba439a640b582b4abfd2c07
Ignacio-momar <ignaciomomar@gmail.com>
2024-07-09 10:09:37 -0400

    mover las clases de la cadr.jsx(deprecada)  a medicationList

--- medicostos/src/components/Card/Card.jsx
+++ medicostos/src/components/Card/Card.jsx
@@ -1,28 +1,30 @@
-import "./Card.css";
+/* esta wea no se ocupa */
 
-const Card = ({
-  imgSrc,
-  altText,
-  title,
-  subtitle,
-  precio,
-  bgColor,
-  onClick,
-}) => {
-  return (
-    <div
-      className="card"
-      style={{ backgroundColor: bgColor }}
-      onClick={onClick}
-    >
-      <img className="imgcard" src={imgSrc} alt={altText}></img>
-      <div className="card-content">
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p className="precio">{precio}</p>
-      </div>
-    </div>
-  );
-};
+// import "./Card.css";
 
-export default Card;
+// const Card = ({
+//   imgSrc,
+//   altText,
+//   title,
+//   subtitle,
+//   precio,
+//   bgColor,
+//   onClick,
+// }) => {
+//   return (
+//     <div
+//       className="card"
+//       style={{ backgroundColor: bgColor }}
+//       onClick={onClick}
+//     >
+//       <img className="imgcard" src={imgSrc} alt={altText}></img>
+//       <div className="card-content">
+//         <h2>{title}</h2>
+//         <h3>{subtitle}</h3>
+//         <p className="precio">{precio}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
