619643c5d1f473c17e26f9ab95b51b8206bebd86
Ignacio-momar <ignaciomomar@gmail.com>
2024-07-09 15:09:02 -0400

    formulario se manda al correo

--- medicostos/src/Contacto/Contacto.jsx
+++ medicostos/src/Contacto/Contacto.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 import "./Contacto.css";
 
 function Contacto() {
-  // Definimos el estado inicial del formulario usando useState.
-  // Este estado guarda los valores de los campos del formulario.
   const [formData, setFormData] = useState({
     nombre: "",
     apellido: "",
@@ -11,8 +10,8 @@ function Contacto() {
     mensaje: "",
   });
 
-  // Función para manejar cambios en los campos del formulario.
-  // Se ejecuta cada vez que el usuario escribe en un campo del formulario.
+  const [confirmada, setConfirmada] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -21,20 +20,29 @@ function Contacto() {
     });
   };
 
-  const [confirmada, setConfirmada] = useState(false); // para que inicie en falso el mensaje de confirmada
-
   const confirmacionClic = () => {
-    /* cambia la confirmada a true */
     setConfirmada(true);
   };
 
-  // Función para manejar el envío del formulario.
-  // Se ejecuta cuando el usuario envía el formulario.
   const handleSubmit = (e) => {
     e.preventDefault(); // Previene el comportamiento por defecto del formulario (recargar la página).
-    console.log("Form data submitted:", formData); // Imprime los datos del formulario en la consola.
-    // alert("consulta enviada, gracias por su aporte"); // Muestra una alerta indicando que el formulario fue enviado.
-    confirmacionClic(); // va aqui el clic, por que? ni perra
+
+    emailjs
+      .send(
+        "service_sx1exjf", // Reemplaza con tu Service ID
+        "template_fil95n9", // Reemplaza con tu Template ID
+        formData,
+        "98u0wS5siZu56Tylr" // Reemplaza con tu User ID
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          confirmacionClic(); // Muestra el mensaje de confirmación
+        },
+        (error) => {
+          console.error("FAILED...", error);
+        }
+      );
   };
 
   return (
@@ -55,9 +63,9 @@ function Contacto() {
                       <input
                         type="text"
                         id="nombre"
-                        name="nombre" // Asocia este textarea con el campo "nombre" en el estado
-                        value={formData.nombre} // Vincula el valor del textarea con el estado actual.
-                        onChange={handleChange} // // Actualiza el estado cuando el usuario escribe.
+                        name="nombre"
+                        value={formData.nombre}
+                        onChange={handleChange}
                         required
                       />
                     </p>
