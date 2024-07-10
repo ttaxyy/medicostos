aa119a2e0a53b8198e21c366b6b64b326b306af5
FabTFlor <fabiantorresflor@gmail.com>
2024-07-10 19:01:42 -0400

    correción de animacion de info card

--- medicostos/src/hooks/useSearchMedications.js
+++ medicostos/src/hooks/useSearchMedications.js
@@ -1,24 +1,28 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchMedications = async (query) => {
+const fetchMedications = async (query, page) => {
   if (!query) {
     return []; // Retorna un array vacío si no hay query
   }
   const { data } = await axios.get(
     "http://localhost:8080/api/medications/search",
     {
-      params: { query, page: 0, size: 50 },
+      params: { query, page, size: 4 },
     }
   );
   return data; // Asegúrate de que data sea un array de medicamentos
 };
 
-export const useSearchMedications = (query) => {
-  return useQuery(["medications", query], () => fetchMedications(query), {
-    enabled: !!query,
-    onError: (error) => {
-      console.error("Error fetching medications:", error);
-    },
-  });
+export const useSearchMedications = (query, page) => {
+  return useQuery(
+    ["medications", query, page],
+    () => fetchMedications(query, page),
+    {
+      enabled: !!query,
+      onError: (error) => {
+        console.error("Error fetching medications:", error);
+      },
+    }
+  );
 };
